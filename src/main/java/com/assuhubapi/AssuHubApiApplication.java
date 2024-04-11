b4fb4ac42c6d66d5f237aa9b8385704e34edef94
Dimic <dimic@example.com>
2024-04-11 13:50:43 +0200

    init

---
+++ src/main/java/com/assuhubapi/AssuHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.assuhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssuHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssuHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assuhubapi/AssuHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assuhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssuHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
